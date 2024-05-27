c2c28a3548d7998e3da6a8fcf342b145102d2c36
U2D-8Bits <u2d.8bits@gmail.com>
2024-05-26 21:38:07 -0500

    WebSockets Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,17 +32,6 @@ import { ConfigModule } from '@nestjs/config';
 
 export class AppModule {
   
-  // Imprimimos en consola toda la información de la base de datos
-  constructor() {
-    console.log(`DB_PORT: ${process.env.PGPORT ?? 3000}`);
-    console.log(`DB_HOST: ${process.env.PGHOST}`);
-    console.log(`DB_PORT: ${process.env.PGPORT}`);
-    console.log(`DB_USER: ${process.env.PGUSER}`);
-    console.log(`DB_PASSWORD: ${process.env.PGPASSWORD}`);
-    console.log(`DB_NAME: ${process.env.POSTGRES_DB}`);
 
-    // Imprimimos en consola la ruta que se debe usar en postman
-    console.log(`http://localhost:${process.env.PORT ?? 3000}/api/role`);
-  }
 
 }
