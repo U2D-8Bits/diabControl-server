a2e8d57709b301f3239164a1e088b1cae1e766ef
U2D-8Bits <u2d.8bits@gmail.com>
2024-06-07 15:55:27 -0500

    ActController Update

--- src/act/act.controller.ts
+++ src/act/act.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ActService } from './act.service';
 import { CreateActDto } from './dto/create-act.dto';
 import { UpdateActDto } from './dto/update-act.dto';
@@ -7,28 +8,47 @@ import { UpdateActDto } from './dto/update-act.dto';
 export class ActController {
   constructor(private readonly actService: ActService) {}
 
+
+  //? Controlador para crear un nuevo Acta
   @Post()
   create(@Body() createActDto: CreateActDto) {
     return this.actService.create(createActDto);
   }
 
+
+  //? Controlador para listar todas las Actas Existentes
   @Get()
   findAll() {
     return this.actService.findAll();
   }
 
+  //? Controlador para obtener el Acta de un Paciente por su ID
+  @Get('/patient/:id')
+  finOneByPatientID(@Param('id', ParseIntPipe) id: number){
+    return this.actService.findOneByPatientId(id)
+  }
+
+
+
+  //? Controlador para obtener un Acta por su ID
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.actService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.actService.findOne(id);
   }
 
+
+
+  //? Controlador para actualizar un acta por su ID
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateActDto: UpdateActDto) {
-    return this.actService.update(+id, updateActDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateActDto: UpdateActDto) {
+    return this.actService.update(id, updateActDto);
   }
 
+
+
+  //? Controlaador para eliminar un Acta por su ID
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     return this.actService.remove(+id);
   }
 }
