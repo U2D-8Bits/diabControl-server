5f9a9a4d17b2b3ac3b20ae29cb50fb353d89f5ae
U2D-8Bits <u2d.8bits@gmail.com>
2024-06-10 14:43:15 -0500

    Medicine Controller Update

--- src/medicines/medicines.controller.ts
+++ src/medicines/medicines.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { MedicinesService } from './medicines.service';
 import { CreateMedicineDto } from './dto/create-medicine.dto';
@@ -7,28 +8,41 @@ import { UpdateMedicineDto } from './dto/update-medicine.dto';
 export class MedicinesController {
   constructor(private readonly medicinesService: MedicinesService) {}
 
+
+  //? Controlador para crear un nuevo medicamento
   @Post()
   create(@Body() createMedicineDto: CreateMedicineDto) {
     return this.medicinesService.create(createMedicineDto);
   }
 
+
+  //? Controlador para listar todos los medicamentos
   @Get()
   findAll() {
     return this.medicinesService.findAll();
   }
 
+
+
+  //? Controlador para buscar un medicamento por su id
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.medicinesService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.medicinesService.findOne(id);
   }
 
+
+
+  //? Controlador para actualizar un medicamento por su id
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMedicineDto: UpdateMedicineDto) {
-    return this.medicinesService.update(+id, updateMedicineDto);
+  update(@Param('id') id: number, @Body() updateMedicineDto: UpdateMedicineDto) {
+    return this.medicinesService.update(id, updateMedicineDto);
   }
 
+
+
+  //? Controlador para eliminar un medicamento por su id
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.medicinesService.remove(+id);
+  remove(@Param('id') id: number) {
+    return this.medicinesService.remove(id);
   }
 }
