39c38f81e910b4be3a338f6c75f92fe99a6fc731
U2D-8Bits <u2d.8bits@gmail.com>
2024-06-10 14:29:43 -0500

    Medicine Dto's Update

--- src/medicines/dto/create-medicine.dto.ts
+++ src/medicines/dto/create-medicine.dto.ts
@@ -1,1 +1,17 @@
-export class CreateMedicineDto {}
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateMedicineDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name_medicine: string;
+
+    @IsString()
+    @IsNotEmpty()
+    generic_name: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    idCategory: number;
+}
--- src/medicines/dto/update-medicine.dto.ts
+++ src/medicines/dto/update-medicine.dto.ts
@@ -1,4 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMedicineDto } from './create-medicine.dto';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateMedicineDto extends PartialType(CreateMedicineDto) {}
+export class UpdateMedicineDto extends PartialType(CreateMedicineDto) {
+    @IsString()
+    @IsOptional()
+    name_medicine?: string;
+
+    @IsString()
+    @IsOptional()
+    generic_name?: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    idCategory: number;
+}
