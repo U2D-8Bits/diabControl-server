c05be1916d948279927d91c12f6f72e5d80590fa
U2D-8Bits <u2d.8bits@gmail.com>
2024-07-28 13:28:05 -0500

    Zoom Service getToken & createMeeting

---
+++ src/zoom/zoom.service.ts
@@ -0,0 +1,61 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, HttpService } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ZoomService {
+  private readonly clientId: string;
+  private readonly clientSecret: string;
+  private readonly accountId: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {
+    this.clientId = this.configService.get<string>('ZOOM_CLIENT_ID');
+    this.clientSecret = this.configService.get<string>('ZOOM_CLIENT_SECRET');
+    this.accountId = this.configService.get<string>('ZOOM_ACCOUNT_ID');
+  }
+
+  private async getAccessToken(): Promise<string> {
+    const response = await this.httpService
+      .post('https://zoom.us/oauth/token', null, {
+        params: {
+          grant_type: 'account_credentials',
+          account_id: this.accountId,
+        },
+        auth: {
+          username: this.clientId,
+          password: this.clientSecret,
+        },
+      })
+      .toPromise();
+    return response.data.access_token;
+  }
+
+  async createMeeting(
+    topic: string,
+    start_time: string,
+    duration: number,
+  ): Promise<any> {
+    const accessToken = await this.getAccessToken();
+    const response = await this.httpService
+      .post(
+        'https://api.zoom.us/v2/users/me/meetings',
+        {
+          topic,
+          type: 2,
+          start_time,
+          duration,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      )
+      .toPromise();
+
+    return response.data;
+  }
+}
