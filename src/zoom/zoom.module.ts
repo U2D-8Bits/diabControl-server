4d4848b4e5401e5db9aa6653f43134fd429e715e
U2D-8Bits <u2d.8bits@gmail.com>
2024-07-28 13:32:46 -0500

    Zoom Module

--- src/zoom/zoom.module.ts
+++ src/zoom/zoom.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { ZoomService } from './zoom.service';
 import { ZoomController } from './zoom.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [ZoomController],
   providers: [ZoomService],
 })
