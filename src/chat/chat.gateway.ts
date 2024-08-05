888b740dcf78499423e53c5c2015d10ad7acb4db
U2D-8Bits <u2d.8bits@gmail.com>
2024-08-05 17:54:29 -0500

    ChatGateway Update

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,59 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
+import { Server, Socket } from 'socket.io';
+import { UsersService } from 'src/users/users.service';
+import { Injectable, UseGuards } from '@nestjs/common';
+import { TokenGuard } from 'src/users/guards/auth/token.guard';
+import { JwtService } from '@nestjs/jwt';
 
-@WebSocketGateway()
-export class ChatGateway {
-  constructor(private readonly chatService: ChatService) {}
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+@Injectable()
+@UseGuards(TokenGuard)
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+  
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  afterInit(server: Server) {
+    console.log('WebSocket initialized');
+  }
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.query.token as string;
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, { secret: process.env.JWT_SEED });
+      const user = await this.usersService.findUserByID(parseInt(payload.id));
+
+      if (user && user.user_status) {
+        client.data.user = user;
+        this.server.emit('userConnected', user);
+        console.log(`User connected: ${user.user_username}`);
+      } else {
+        client.disconnect();
+      }
+    } catch (error) {
+      client.disconnect();
+    }
+  }
+
+  handleDisconnect(client: Socket) {
+    const user = client.data.user;
+    if (user) {
+      this.server.emit('userDisconnected', user);
+      console.log(`User disconnected: ${user.user_username}`);
+    }
+  }
+
 }
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [ChatGateway, ChatService],
+  // eslint-disable-next-line prettier/prettier
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UsersModule
+  ],
+  providers: [ChatGateway, ChatService, JwtService],
 })
 export class ChatModule {}
